6dac3eace56da1bf091ad05190ce07af07366389
zit-git-jzan <johann@zandanel.net>
2024-12-23 22:26:44 +0100

    Upgrade

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-westeurope"
-    storage_account_name = "csb1003200045814e8e"
+    resource_group_name  = "cloud-shell-storage"
+    storage_account_name = "mycloudshellstorag"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
 
